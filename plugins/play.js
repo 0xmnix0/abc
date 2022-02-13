2ac986f4112a3de9701ad68e9e0951f9fe393937
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2022-02-13 22:08:02 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -46,8 +46,8 @@ ${vid.url}
            hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
-               displayText: 'DONASI',
-               url: 'https://saweria.co/ilmanhdyt',
+               displayText: 'Owner',
+               url: 'https://wa.me/6281252848955',
              }
 
            },
